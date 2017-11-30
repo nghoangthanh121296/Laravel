50e0ee1e074537c2d2d09fbd37399c05c0bd91db
NguyenHoangThanh <nghoangthanh121296@gmail.com>
2017-11-30 18:18:25 +0700

    Homepage

--- resources/views/User/Home.blade.php
+++ resources/views/User/Home.blade.php
@@ -1,44 +1,26 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>yeah</title>
+	<title></title>
 	<meta property="og:type" content="website">
 	<meta property="og:url" content="https://www.flightclub.com/">
 	<meta property="og:title" content="Sneakers. Here.  | Flight Club">
 
 	<link href="{{ asset('bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+	<!-- <link href="{{ asset('css/custom.css') }}" rel="stylesheet"> -->
+	<link href="{{ asset('css/custom.css') }}" rel="stylesheet">
 
 </head>
 <body>
-<nav class="navbar navbar-default">
+<nav class="navbar navbar-default" style="margin-bottom: 0px;">
   <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
       <a class="navbar-brand" href="#">Brand</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        
-        
-          <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">One more separated link</a></li>
-          </ul>
-        </li>
-      </ul>
       <form class="navbar-form navbar-left">
         <div class="form-group">
           <input type="text" class="form-control" placeholder="Search">
@@ -46,75 +28,194 @@
         <button type="submit" class="btn btn-default">Submit</button>
       </form>
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Link</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-          </ul>
-        </li>
+        <li><a href="#">Sign-in</a></li>
+        <li><a href="#">Sign-out</a></li>
+        
       </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
 </nav>
-<nav class="navbar navbar-inverse">
+<nav class="navbar navbar-default ">
   <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="#">Brand</a>
-    </div>
-
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        
-        
-          <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">One more separated link</a></li>
-          </ul>
-        </li>
-      </ul>
-      <form class="navbar-form navbar-left">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Search">
-        </div>
-        <button type="submit" class="btn btn-default">Submit</button>
-      </form>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Link</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
+  	<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+   <ul class="nav navbar-nav">
+   	<li><a href="#">AIR JORDANS </a> </li>
+   <li>	<a href="#">NIKE</a>   </li>
+  <li><a href="#"> ADIDAS  </a> </li>
+<li><a href="#">YEEZY  </a> </li>
+<li><a href="#">FOOTWEAR </a>  </li>
+<li><a href="#">APPAREL  </a> </li>
+<li><a href="#">ACCESSORIES  </a> </li>
+<li><a href="#">NEW ARRIVALS </a>  </li>
+<li><a href="#">GIFT CARDS </a>  </li>
+<li><a href="#">PRICE DROPS</a> </li>
+   </ul>
+</div>
+    
+  </div>
 </nav>
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="{{ asset('img/banner/PriceDropped.jpg') }}" alt="...">
+      <div class="carousel-caption">
+        
+      </div>
+    </div>
+    <div class="item">
+      <img src="{{ asset('img/banner/Anniversary.jpg') }}" alt="...">
+      <div class="carousel-caption">
+        
+      </div>
+    </div>
+    <div class="item">
+      <img src="{{ asset('img/banner/Ridetheway.jpg') }}" alt="...">
+      <div class="carousel-caption">
+        
+      </div>
+    </div>
+    <div class="item">
+      <img src="{{ asset('img/banner/Allweeklong.jpg') }}" alt="...">
+      <div class="carousel-caption">
+        
+      </div>
+    </div>
+    
+  </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+<h2 align="center">FIND YOUR PAIR</h2>
+<div class="row">
+	<div class="col-md-4" align="center"><img src="{{ asset('img/category/Men.jpg') }}" style="width: 350px;">
+			<h2>MEN</h2>
+	</div>
+	
+	<div class="col-md-4" align="center"><img src="{{ asset('img/category/Women.jpg') }}" style="width: 350px;">
+	<h2>WOMEN</h2></div>
+	
+	<div class="col-md-4" align="center"><img src="{{ asset('img/category/Kids.jpg') }}" style="width: 350px;">
+	<h2>KIDS</h2></div>
+	
+</div>
+<h2 align="center">NEW ARRIVALS</h2>
+<div class="container-fluid">
+
+<div class="col-lg-3 col-md-2 col-sm-6 col-xs-12">
+    <div class="hovereffect">
+        <a class="info" href="#" title="air jordan 13 retro sngl day" style="text-decoration:none">
+        	<img class="img-responsive" src="{{ asset('img/product/airjordan13.jpg') }}" alt="">
+        <div class="overlay">
+           <h2>AIR JORDAN </h2>
+           <span>air jordan 13 retro sngl day</span></br>
+           <span>$250</span>
+        </div>
+        </a>
+    </div>
+</div>
+<div class="col-lg-3 col-md-2 col-sm-6 col-xs-12">
+    <div class="hovereffect">
+        <a class="info" href="#" title="air jordan 13 retro sngl day" style="text-decoration:none">
+        	<img class="img-responsive" src="{{ asset('img/product/airjordan13.jpg') }}" alt="">
+        <div class="overlay">
+           <h2>AIR JORDAN </h2>
+           <span>air jordan 13 retro sngl day</span></br>
+           <span>$250</span>
+        </div>
+        </a>
+    </div>
+</div>
+<div class="col-lg-3 col-md-2 col-sm-6 col-xs-12">
+    <div class="hovereffect">
+        <a class="info" href="#" title="air jordan 13 retro sngl day" style="text-decoration:none">
+        	<img class="img-responsive" src="{{ asset('img/product/airjordan13.jpg') }}" alt="">
+        <div class="overlay">
+           <h2>AIR JORDAN </h2>
+           <span>air jordan 13 retro sngl day</span></br>
+           <span>$250</span>
+        </div>
+        </a>
+    </div>
+</div>
+<div class="col-lg-3 col-md-2 col-sm-6 col-xs-12">
+    <div class="hovereffect">
+        <a class="info" href="#" title="air jordan 13 retro sngl day" style="text-decoration:none">
+        	<img class="img-responsive" src="{{ asset('img/product/airjordan13.jpg') }}" alt="">
+        <div class="overlay">
+           <h2>AIR JORDAN </h2>
+           <span>air jordan 13 retro sngl day</span></br>
+           <span>$250</span>
+        </div>
+        </a>
+    </div>
+</div>
+<div hidden id="show1">
+<div class="col-lg-3 col-md-2 col-sm-6 col-xs-12" >
+    <div class="hovereffect">
+        <a class="info" href="#" title="air jordan 13 retro sngl day" style="text-decoration:none">
+        	<img class="img-responsive" src="{{ asset('img/product/airjordan13.jpg') }}" alt="">
+        <div class="overlay">
+           <h2>AIR JORDAN </h2>
+           <span>air jordan 13 retro sngl day</span></br>
+           <span>$250</span>
+        </div>
+        </a>
+    </div>
+</div>
+</div>
+
+
+
+<div class="col-md-12" style="text-align:center;">
+<button type="button" id="show" value="more" class="btn btn-default">SHOW MORE</button>
+
+</div>
+</div>
+<h2 align="center">RECOMMENDATIONS FOR YOU</h2>
+
+
 </body>
+
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="{{ asset('bootstrap/js/bootstrap.min.js') }}"></script>
+ <script>
+$(document).ready(function(){
+    $("#show").click(function(){
+    	var now = $("#show").val();
+    	if( now == "more")
+    	{
+        	//alert(now);
+        	$("#show").val("hide");
+        	$("#show").text("SHOW LESS");
+        	$("#show1").show();
+    	}
+    	else
+    	{
+			//alert(now);
+			$("#show").val("more");
+			$("#show").text("SHOW MORE");
+			$("#show1").hide();
+    	}
+    });
+});
+</script>
 </html>
